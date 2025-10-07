4504723cd47990937dca4a46d7016d18e24f692e
Mavis <mavisgundaya@gmail.com>
2025-10-08 02:34:22 +0800

    ++ fixe otp filing

--- frontend/src/app/auth/verify-otp/page.tsx
+++ frontend/src/app/auth/verify-otp/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Suspense } from "react";
-import OtpPageContent from "./OtpPageContent";
+import OtpPageContent from "./OtpPageContent/page";
 
 export const dynamic = "force-dynamic";
 
